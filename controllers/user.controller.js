a6101b89847d5110366906b92be24ba24ad0e936
rakiful <mr143131@gmail.com>
2025-08-17 19:29:45 +0600

    added update user controller function

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -67,9 +67,77 @@ const updateUserRole = async (req, res) => {
   }
 };
 
+// Get single user by email
+const getUserByEmail = async (req, res) => {
+  try {
+    const { email } = req.params;
+    const user = await req.db.userCollections.findOne({ email });
+    if (!user) return res.status(404).send({ message: "User not found" });
+    res.send(user);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: "Internal Server Error", error: error.message });
+  }
+};
+
+// Update User Profile
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const {
+      name,
+      nickName,
+      gender,
+      country,
+      address,
+      language,
+      timezone,
+      photo,
+    } = req.body;
+
+    // Build the update object dynamically
+    const updateFields = {};
+    if (name) updateFields.name = name;
+    if (nickName) updateFields.nickName = nickName;
+    if (gender) updateFields.gender = gender;
+    if (country) updateFields.country = country;
+    if (address) updateFields.address = address;
+    if (language) updateFields.language = language;
+    if (timezone) updateFields.timezone = timezone;
+    if (photo) updateFields.photo = photo;
+
+    if (Object.keys(updateFields).length === 0) {
+      return res
+        .status(400)
+        .send({ success: false, message: "No fields provided for update" });
+    }
+
+    const result = await req.db.userCollections.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updateFields }
+    );
+
+    if (result.matchedCount === 0) {
+      return res
+        .status(404)
+        .send({ success: false, message: "User not found" });
+    }
+
+    res
+      .status(200)
+      .send({ success: true, message: "User updated successfully" });
+  } catch (err) {
+    console.error("Update User Error:", err);
+    res.status(500).send({ success: false, message: err.message });
+  }
+};
+
 module.exports = {
   getUsers,
+  getUserByEmail,
   addOrUpdateUser,
   getUserRole,
   updateUserRole,
+  updateUser,
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,16 +1,15 @@
 // routes/user.routes.js
 const express = require("express");
 
-const {
-  verifyAdmin,
-  verifyToken,
-} = require("../middlewares/auth.middlewares");
+const { verifyAdmin, verifyToken } = require("../middlewares/auth.middlewares");
 
 const {
   getUsers,
+  getUserByEmail,
   addOrUpdateUser,
   getUserRole,
   updateUserRole,
+  updateUser,
 } = require("../controllers/user.controller");
 
 module.exports = (db) => {
@@ -25,12 +24,9 @@ module.exports = (db) => {
   router.get("/users", verifyToken, verifyAdmin, getUsers);
   router.get("/users/:email/role", getUserRole);
   router.post("/users", addOrUpdateUser);
-  router.patch(
-    "/users/role/:id",
-    verifyToken,
-    verifyAdmin,
-    updateUserRole
-  );
+  router.patch("/users/role/:id", verifyToken, verifyAdmin, updateUserRole);
+  router.patch("/users/:id", verifyToken, updateUser);
+  router.get("/users/email/:email", verifyToken, getUserByEmail);
 
   return router;
 };
